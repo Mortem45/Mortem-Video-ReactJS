9b2642a7969006cec688e03fb5381a58d3084730
Mortem45 <ste45_007@hotmail.com>
2019-01-26 12:57:06 -0600

    layout base

---
+++ src/pages/containers/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import HomeLayout from '../components/home-layout'
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <HomeLayout>Hola</HomeLayout>
+      </div>
+    )
+  }
+}
