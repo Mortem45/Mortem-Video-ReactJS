50ff0b85184c4668d58d225f8092f57fa1250d7e
Mortem45 <ste45_007@hotmail.com>
2019-01-27 14:39:32 -0600

    agregando buscador

---
+++ src/widgets/components/search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './search.css';
+const Search = () => (
+  <form action="" className="Search">
+    <input
+      type="text"
+      placeholder="Busca tu pelicula favorita"
+      className="Search-input" />
+  </form>
+)
+
+export default Search;
+
