24d174311197835ae1594af7b9b16407657b7649
Mortem45 <ste45_007@hotmail.com>
2019-01-27 15:54:59 -0600

    mejorando componentes

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import VideoPlayerLayout from '../components/video-player-layout';
+import Video from '../containers/video'
+import Title from '../components/title'
+
 export default class VideoPlayer extends Component {
   render() {
     return (
       <VideoPlayerLayout>
-        <video
-          controls
-          autoPlay
+        <Title
+          title="este es un video"
+        />
+        <Video
+          autoPlay={this}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
-
-          />
+        />
       </VideoPlayerLayout>
     )
   }
